a45fb6eb5b9a7a6701cd51a7feec9eb31ae6d686
Carlos Henricke A.A. <carloshenricke2005@hotmail.com>
2025-08-21 22:36:58 -0300

    a

--- scrips.js
+++ scrips.js
@@ -1,1 +1,15 @@
 console.log("ola mundo");
+//criar metodos para criar, deletar , modificar e mostrar tarefas
+
+function criarTarefa() {
+
+}
+function showTarefas() {
+
+}
+function deletarTarefa() {
+
+}
+function modificarTarefa() {
+    
+}
